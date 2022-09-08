e6835feb19f962ac06a44bbde52cbee835553197
lambtron <andyjiang@gmail.com>
2022-09-08 11:44:52 -0700

    test

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,19 @@
 
-export default function Home() {
+export const handler = {
+  async GET(_, ctx) {
+    const ip = (ctx.remoteAddr as Deno.NetAddr).hostname;
+    const res = await fetch("http://ip-api.com/json/" + ip);
+    const data = await res.json();
+    console.log(data);
+    return ctx.render(data);
+  },
+};
+
+export default function Home({ city, country }) {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <p class="my-6">
-        Hello, twitter!
+        Hello, twitter from {city}, {country}!
       </p>
     </div>
   );
