36cfb9a1e16f976175be7605372b8e805deede85
lambtron <andyjiang@gmail.com>
2022-09-08 16:35:18 -0700

    say hi

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <p class="my-6">
-        <h1 class="text-6xl">Hi, twitter!</h1>
+        <img src="hi-twitter.png" />
       </p>
     </div>
   );
