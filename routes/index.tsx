09e959e865d30d1e13b9c7365ed801bf18a96fae
lambtron <andyjiang@gmail.com>
2022-09-12 10:01:59 -0700

    hello

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div class="p-4 mx-auto max-w-screen-md">
       <p class="my-6">
-        <img src="hi-twitter.png" />
+        Hello, world!
       </p>
     </div>
   );
